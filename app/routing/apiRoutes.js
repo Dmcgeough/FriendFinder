000c970d8d2819d2020242cc9ed73ab75e7a484c
Dmcgeough <danny.mcgeough@gmail.com>
2017-08-26 16:45:14 -0400

    final

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,11 +2,13 @@ var friends = require ("../data/friends.js")
 module.exports = function(app){
 
     app.get("/api/friends", function(req, res) {
-        res.json(friends);
+        return res.json(friends);
     })
 
     app.post("/api/friends", function(req, res) {
         var content = req.body;
-        res.json(content);
+        friends.push(content);
+        console.log(content);
+        res.json(friends);
     });
 }
